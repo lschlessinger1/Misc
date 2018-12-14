d5c77eb11f6db867faa7359599348d28b0762b51
Louis Schlessinger <louschlessinger@gmail.com>
2018-12-13 21:42:22 -0600

    fixes #1

--- parabolic_interpolation/parabolicInterp.m
+++ parabolic_interpolation/parabolicInterp.m
@@ -1,5 +1,13 @@
 function extremum = parabolicInterp(x1, x2, x3, y1, y2, y3)
-    % parabolicInterp computes the parabolic extremum given three points.
+    % parabolicInterp computes the parabolic extremum given three 2D 
+    % points.
+    
+    % check to see if the three points are collinear
+    triangleArea = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+    if triangleArea == 0
+        error(['Cannot compute parabolic interpolation with three' ...
+        ' collinear points']);
+    end
     
     numerator = (x2^2 - x1^2) * (y2 - y3) - (x2^2 - x3^2) * (y2 - y1);
     denomominator = (x2 - x1) * (y2 - y3) - (x2 - x3) * (y2 - y1);
