73bbc01f908e14077436aadf05582cdebdff96b0
Louis Schlessinger <louschlessinger@gmail.com>
2018-12-13 21:58:43 -0600

    fix #2

--- parabolic_interpolation/parabolicInterp.m
+++ parabolic_interpolation/parabolicInterp.m
@@ -1,7 +1,9 @@
-function extremum = parabolicInterp(x1, x2, x3, y1, y2, y3)
-    % parabolicInterp computes the parabolic extremum given three 2D 
-    % points.
-    
+function [extremum, is_maximum] = parabolicInterp(x1, x2, x3, y1, y2, y3)
+% parabolicInterp computes parabolic interpolation extremum given three 2D 
+% points.
+% is_maximum: 0 if extremum is minimum, 1 if extremum is maximum
+%
+
     % check to see if the three points are collinear
     triangleArea = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
     if triangleArea == 0
@@ -9,7 +11,17 @@ function extremum = parabolicInterp(x1, x2, x3, y1, y2, y3)
         ' collinear points']);
     end
     
+    % compute extremum
     numerator = (x2^2 - x1^2) * (y2 - y3) - (x2^2 - x3^2) * (y2 - y1);
     denomominator = (x2 - x1) * (y2 - y3) - (x2 - x3) * (y2 - y1);
     extremum = (1/2) * (numerator / denomominator);
+    
+    % determine if extremum is minimum or maximum
+    a = x1 * (y3 - y2) + x2 *(y1 - y3) + x3 * (y2 - y1) / (x1 - x2) * ...
+        (x1 - x3) * (x2 - x3);
+    if a > 0
+        is_maximum = 0;
+    else
+        is_maximum = 1;
+    end
 end
